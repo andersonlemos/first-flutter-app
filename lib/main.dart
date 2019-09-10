1df239f24524dcfe7d77faadfa6bc244c5a01184
Anderson Lemos <bdl182@gmail.com>
2019-09-10 17:05:27 -0300

    update SharedPreferences package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:flutter/src/widgets/binding.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'models/item.dart';
 
@@ -31,9 +34,9 @@ class HomePage extends  StatefulWidget{
 
     items = [];
 
-    items.add(Item(title: "Banana",done: false));
-    items.add(Item(title: "Abacate",done: true));
-    items.add(Item(title: "Laranja",done: false));
+    // items.add(Item(title: "Banana",done: false));
+    // items.add(Item(title: "Abacate",done: true));
+    // items.add(Item(title: "Laranja",done: false));
   }
 
   @override
@@ -44,7 +47,7 @@ class HomePage extends  StatefulWidget{
 class _HomePage extends State<HomePage>{
   
   TextEditingController taskController = new TextEditingController();
-  
+
   void add(){
     
     if(taskController.text.isEmpty) return; 
@@ -52,9 +55,47 @@ class _HomePage extends State<HomePage>{
     setState(() {
        widget.items.add(Item(title: taskController.text, done: false));
        taskController.clear();
+       save();
     });
+  
   }
 
+  void remove(int index){
+    setState(() {
+     widget.items.removeAt(index); 
+     save();
+    });
+  
+  }
+  
+  Future load() async{
+    
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if(data != null){
+
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x)=> Item.fromJson(x)).toList();
+
+      setState(() {
+       widget.items = result; 
+      });
+    }
+
+  }
+
+  save() async{
+    var prefs =  await SharedPreferences.getInstance();
+
+    await prefs.setString('data',jsonEncode(widget.items));
+    print(widget.items);
+    print(prefs);
+  }
+
+   _HomePage(){
+     load();
+   }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -76,17 +117,28 @@ class _HomePage extends State<HomePage>{
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index){
           final item = widget.items[index];
-          return CheckboxListTile(
-            key: Key(item.title),
-            title: Text(item.title),
-            value: item.done,
-            onChanged: (value){
-              setState(() {
-               item.done = value; 
-              });
-            },
-
+          return Dismissible(
+            child: CheckboxListTile(
+                  title: Text(item.title),
+                  value: item.done,
+                  onChanged: (value){
+                    setState(() {
+                    item.done = value;
+                    save(); 
+                    });
+                    
+                  },
+               ),
+               key: Key(item.title),
+               background: Container(
+                 color: Colors.blueGrey.withOpacity(0.3),
+              ),
+              onDismissed: (direction){
+                remove(index);
+              },
+            
           );
+          
         }
       ),
       floatingActionButton: FloatingActionButton(
